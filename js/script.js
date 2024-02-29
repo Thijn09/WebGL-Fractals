83897a4d8698880f9ab5c5eb7956763d2e066007
Thijn09 <131200125+Thijn09@users.noreply.github.com>
2024-02-29 17:38:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,9 +35,9 @@ function livelyPropertyListener(name, val) {
     case "color":
       options.color = hexToRgb(val);
       meshGeometry.material.uniforms.u_color.value = new THREE.Vector3(
-        options.color.r,
-        options.color.g,
-        options.color.b
+        options.color.r / 255,
+        options.color.g / 255,
+        options.color.b / 255
       );
       break;
   }
